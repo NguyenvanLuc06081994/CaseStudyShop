342e19243ef39aae46e95df9bd79b21a26e89430
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-07-25 09:51:27 +0700

    add feature get all Categories

--- admin/src/Model/CategoryManager.php
+++ admin/src/Model/CategoryManager.php
@@ -14,4 +14,18 @@ class CategoryManager
         $this->database = $db->connect();
     }
 
+    public function getAllCategory()
+    {
+        $sql = "SELECT * FROM  tbl_categories";
+        $stmt = $this->database->query($sql);
+        $data = $stmt->fetchAll();
+        $categories = [];
+        foreach ($data as $key => $item){
+            $category = new Category($item['name'],$item['country']);
+            $category->setId($item['id']);
+            array_push($categories,$category);
+        }
+        return $categories;
+    }
+
 }
