672aff98c7adae0f6e2d1ccc7992fc27018a7a83
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-07-26 22:38:03 +0700

    update

--- admin/src/View/login.php
+++ admin/src/View/login.php
@@ -1,1 +1,92 @@
 <?php
+
+use App\Model\DBConnect;
+
+try {
+    $db = new DBConnect();
+    $conn = $db->connect();
+    if (isset($_POST["login"])) {
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
+            $message = '<label>All fields are required</label>';
+        } else {
+            $sql = "SELECT * FROM tbl_users WHERE username = :username AND password = :password";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(
+                [
+                    'username' => $_POST["username"],
+                    'password' => $_POST["password"]
+                ]
+            );
+
+            $count = $stmt->rowCount();
+            if ($count > 0) {
+                $_SESSION["username"] = $_POST["username"];
+                $_SESSION["password"] = $_POST["password"];
+                header("location:index.php");
+            } else {
+                $message = '<label>Wrong Data</label>';
+            }
+        }
+    }
+} catch (PDOException $error) {
+    $message = $error->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-login.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+        <div class="card">
+            <div class="card-header">
+                <h3>Sign In</h3>
+                <?php
+                if (isset($message)) {
+                    echo '<label class="text-danger">' . $message . '</label>';
+                }
+                ?>
+            </div>
+            <div class="card-body">
+                <form method="post">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" name="username" class="form-control" placeholder="username">
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" name="password" class="form-control" placeholder="password">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="login" value="Login" class="btn float-right login_btn">
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Don't have an account?<a href="#">Sign Up</a>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
