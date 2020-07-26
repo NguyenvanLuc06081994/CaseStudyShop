3d66134b95cc636706604fc8102fc470ee524c19
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-07-26 17:43:42 +0700

    add  feature update customer

--- admin/src/View/customer/update.php
+++ admin/src/View/customer/update.php
@@ -1,1 +1,17 @@
-<?php
+ <a href="index.php?page=list-customer" class="btn btn-primary">Cancel</a>
+<form action="" method="post" class="form-control">
+    <input type="text" name="id" placeholder="Name" value="<?php echo $customer['id'] ?>"  class="form-control" hidden>
+    <br>
+    <input type="text" name="name" placeholder="Name" value="<?php echo $customer['name'] ?>" class="form-control">
+    <br>
+    <input type="text" name="phone" placeholder="Phone Number" value="<?php echo $customer['phone'] ?>" class="form-control">
+    <br>
+    <input type="text" name="email" placeholder="Email" value="<?php echo $customer['email'] ?>" class="form-control">
+    <br>
+    <input type="text" name="address" placeholder="Address" value="<?php echo $customer['address'] ?>" class="form-control">
+    <br>
+    <input type="submit" value="UPDATE" class="btn btn-primary">
+</form>
+
+
+
